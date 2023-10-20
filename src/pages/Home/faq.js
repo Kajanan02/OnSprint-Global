a3c6dccabe19157f5b87094b19f4a3c83e003f3d
Yamsikan Yoganathan <64594344+Yamsikan-Yoganathan@users.noreply.github.com>
2023-10-20 13:51:22 +0530

    about-change

--- src/pages/Home/faq.js
+++ src/pages/Home/faq.js
@@ -10,7 +10,7 @@ function Faq(props) {
                 <div className="container-fluid col-10 col-md-9 col-xl-8 px-0 py-5 align-items-center">
                     <h2 className="text-center mb-md-5 mb-3">Frequenly Asked Questions</h2>
                     {faq.map((data, index) => (
-                        <Col key={index} className="gap-1 faq-bar">
+                        <Col key={index} className="faq-bar">
                             <Faqprops data={data}/>
                         </Col>
                     ))}
