559a6e248e9802527894505698564f8f458e3a8b
Kajanan02 <Kajanan02000@gmail.com>
2023-10-24 17:26:14 +0530

    changes

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { Col, Form, Row } from "react-bootstrap";
 import Banner from "../../components/banner";
+import {toast} from "react-toastify";
+import Loader from "../../assets/loader";
 
 function Contact() {
+
+  const [formData, setFormData] = useState({})
+  const [loading, setLoading] = useState(false)
+  function sendMsg(e) {
+
+    setLoading(true)
+    e.preventDefault()
+    fetch("https://api.apispreadsheets.com/data/BSVJzORUniLXlR99/", {
+      method: "POST",
+      body: JSON.stringify({"data": formData}),
+    }).then(res =>{
+      if (res.status === 201){
+        // SUCCESS
+        toast.success("Successfully sent the message")
+        setFormData({})
+      }
+      else{
+        // ERROR
+        toast.error("Something went wrong")
+
+      }
+      setLoading(false)
+
+    })
+  }
+
+  function handleChange(e) {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
   return (
     <>
+      <Loader visible={loading}/>
       <Banner
         className={"banner banner-contact"}
         title={"Contact Page"}
@@ -13,25 +46,25 @@ function Contact() {
       <div className="container-fluid col-10 col-md-9 col-xl-8 p-0">
         <Row className="py-6 justify-content-between align-items-center">
           <Col xl={4} md={4} sm={12}>
-            <h3 class="mb-4 mt-4">Contact US</h3>
-            <p class="mb-2 ">
+            <h3 className="mb-4 mt-4">Contact US</h3>
+            <p className="mb-2 ">
               Contrary to popular belief, Lorem Ipsum is not simply random text.
               It has roots in a piece of classical
             </p>
 
-            <div class="con-detail mt-4">
-              <div class="d-flex gap-2">
-                <span class="material-symbols-rounded text-primary">call</span>
+            <div className="con-detail mt-4">
+              <div className="d-flex gap-2">
+                <span className="material-symbols-rounded text-primary">call</span>
                 <p>(+94) 701453919</p>
               </div>
 
-              <div class="d-flex gap-2 mt-2">
-                <span class="material-symbols-rounded text-primary">mail</span>
+              <div className="d-flex gap-2 mt-2">
+                <span className="material-symbols-rounded text-primary">mail</span>
                 <p>onsprintglobal@gmail.com</p>
               </div>
 
-              <div class="d-flex gap-2 mt-2">
-                <span class="material-symbols-rounded text-primary">
+              <div className="d-flex gap-2 mt-2">
+                <span className="material-symbols-rounded text-primary">
                   location_on
                 </span>
                 <p>SriLanka</p>
@@ -40,14 +73,14 @@ function Contact() {
           </Col>
 
           <Col xl={7} md={7} sm={12}>
-            <Form>
+            <Form onSubmit={sendMsg}>
               <Form.Group className="mb-4 mt-2">
                 <Form.Label>Full Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Name" />
+                <Form.Control type="text"  placeholder="Enter Name" value={formData.fullName} required name={"fullName"} onChange={handleChange}/>
               </Form.Group>
-              <Form.Group class="mb-4">
+              <Form.Group className="mb-4">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" value={formData.email} required  name={"email"} onChange={handleChange}/>
               </Form.Group>
               <Form.Group className="mb-4">
                 <Form.Label>Message</Form.Label>
@@ -55,9 +88,12 @@ function Contact() {
                   as="textarea"
                   placeholder="Enter message"
                   rows={3}
+                  name={"message"} value={formData.message}
+                  required
+                  onChange={handleChange}
                 />
               </Form.Group>
-              <button type="button" className="btn btn-primary ">
+              <button type="submit" className="btn btn-primary ">
                 Send Message
               </button>
             </Form>
--- src/pages/services/service-card.js
+++ src/pages/services/service-card.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ServiceCard(props) {
   const { heading, para } = props.data;
+  const [expand, setExpand] = useState(false);
+  function expanding() {
+      setExpand(!expand)
+  }
   return (
     <div className={"services-card"}>
       <div className={"service-card-heading"}>{heading}</div>
-      <div className={"service-card-para"}>{para}</div>
-      <div className={"service-card-more-details"}>{"More Details >"}</div>
+      <div className={"service-card-para " + (expand ? "service-card-para-visible":" ")}>{para}</div>
+      <div className={"service-card-more-details"} onClick={expanding}>{expand ? "Less Details >":"More Details >"}</div>
     </div>
   );
 }
